d5c740e88fd3ca33abde88f4c007b89358aa706a
zakariasan <elitziko@gmail.com>
2023-12-23 16:04:47 +0100

    0x00. Fix my code1

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -21,7 +21,7 @@ def fizzbuzz(n):
             tmp_result.append("Fizz")
         elif (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 5) == 0 and (i % 5) != 0:
+        elif (i % 5) == 0 and (i % 3) != 0:
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
