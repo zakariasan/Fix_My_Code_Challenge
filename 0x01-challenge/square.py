8eae80fb9009ad0590904057f95f5b58863e8fef
zakariasan <elitziko@gmail.com>
2024-02-11 23:02:45 +0100

    fix my code challange

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+class square():
+
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """ init stuff of function """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        """ Permiter Of the Square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ Print the with and height of aquare """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
