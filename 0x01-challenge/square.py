58d016ff1adbc8ebb039091864ce7fc754863486
zakariasan <elitziko@gmail.com>
2024-02-12 08:08:10 +0100

    fix comment in square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Class module for square application """
 
 
-class square():
+class Square():
     """ square for printing and calclulating square """
     width = 0
     height = 0
@@ -16,7 +16,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """ Permiter Of the Square """
         return (self.width * 2) + (self.height * 2)
 
@@ -26,8 +26,8 @@ class square():
 
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    """ Exemple of my square """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
